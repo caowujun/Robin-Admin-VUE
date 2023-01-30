c631e25d009a56e50725a09d4c4e79644d1165b4
caowujun <baaigeini@163.com>
2023-01-30 19:33:25 +0800

    perf: 不清楚为什么(config: AxiosRequestConfig)这报错。加了个any

--- src/config/axios/service.ts
+++ src/config/axios/service.ts
@@ -24,7 +24,7 @@ const service: AxiosInstance = axios.create({
 
 // request拦截器
 service.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
+  (config: AxiosRequestConfig | any) => {
     if (
       config.method === 'post' &&
       (config.headers as AxiosRequestHeaders)['Content-Type'] ===
